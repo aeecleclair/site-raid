215808e0060d8282dd038eafd5c799d022823292
Maxime Roucher <maximeroucher@gmail.com>
2023-08-28 19:45:40 +0200

    Adding avant gout carousel

--- src/components/Raid/avantgout.jsx
+++ src/components/Raid/avantgout.jsx
@@ -1,26 +1,36 @@
 import { useState } from "react";
+import { Carousel } from "react-bootstrap";
 
 export default function AvantGout() {
-    const [cached, setCached] = useState(false);
-    const [data, setData] = useState([]);
-    if (!cached) {
-      fetch("assets/Raid/avantgout/avantgout.json")
-        .then((res) => res.json())
-        .then((json) => setData(json))
-        .catch(function (err) {
-          console.log(err, " error");
-        })
-        .then(() => setCached(data !== undefined))
-        .then(() => console.log("data fetched avantgout"));
-    }
+  const [cached, setCached] = useState(false);
+  const [data, setData] = useState([]);
+  if (!cached) {
+    fetch("assets/Raid/avantgout/avantgout.json")
+      .then((res) => res.json())
+      .then((json) => setData(json))
+      .catch(function (err) {
+        console.log(err, " error");
+      })
+      .then(() => setCached(data !== undefined))
+      .then(() => console.log("data fetched avantgout"));
+  }
   return (
-    <div className="view under-navbar">
+    <div className="view full-page">
       <h1>L'AVANT-GOUT</h1>
-      <img
-        src={process.env.PUBLIC_URL + "assets/Raid/avantgout/" + data.img}
-        alt=""
-        className="panorama"
-      />
+      {data.data !== undefined && data.data.length !== 0 && (
+        <Carousel>
+          {data.data.map((d, index) => {
+            return (
+              <Carousel.Item>
+                <img
+                  className="panorama"
+                  src={process.env.PUBLIC_URL + "assets/Raid/avantgout/" + d}
+                />
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+      )}
       <h4 className="panorama-descr">{data.description}</h4>
     </div>
   );
