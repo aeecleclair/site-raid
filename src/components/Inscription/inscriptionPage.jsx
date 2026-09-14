e3ff9cac1cbe6fc1456cd8c34960f2051cebaef8
AdrienRivet <adrien.rivet@etu.ec-lyon.fr>
2026-09-14 09:47:53 +0200

    Ajout lien pour trouver binome

--- src/components/Inscription/inscriptionPage.jsx
+++ src/components/Inscription/inscriptionPage.jsx
@@ -123,6 +123,25 @@ export default function InscriptionPage() {
             ) : (
               <div></div>
             )}
+            {data.lienGGSheetsBinome !== "" ? (
+              <div className="lien">
+              <h1>Trouver un binôme</h1>
+                <h5>
+                  Des problèmes pour trouver un binôme ? {" "}
+                  <i>Trouve ton partenaire en t'inscrivant sur ce</i>{" "}
+                  <a
+                    href={data.lienGGSheetsBinome}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i>Google Sheets</i>
+                  </a>
+                  <div style={{ 'height': "60px" }}></div>
+                </h5>
+              </div>
+            ) : (
+              <></>
+            )}
             {data.lienBen !== "" ? (
               <div className="lien">
               <h1>Pour être Bénévole</h1>
